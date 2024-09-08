17f5555a545e08799b5d69d3ecb47c5b75522717
Wj12344 <jinwang8@student.unimelb.edu.au>
2024-09-08 21:52:23 +1000

    Homepage.vue: /*  frequent ask Question */

--- itp-front/src/views/HomePage.vue
+++ itp-front/src/views/HomePage.vue
@@ -1,40 +1,37 @@
 <template>
-    <div id="HomePage">
-      <Header></Header>
-      <MainBanner></MainBanner>
-      <MainFooter></MainFooter>
-      <main class="content">
-      
-        <router-view></router-view>
-      </main>
-      
-      
-    </div>
-  </template>
+  <div id="HomePage">
+    <Header></Header>
+    <MainBanner></MainBanner>
+    <ask-menu></ask-menu>
+    <MainFooter></MainFooter>
+    <main class="content">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
 
 <script>
-import Header from '../components/Header.vue';
-import MainBanner from '../components/MainBannerHome.vue'
-import MainFooter from '../components/MainFooter.vue';
-
-
+import Header from "../components/Header.vue";
+import MainBanner from "../components/MainBannerHome.vue";
+import AskMenu from "../components/AskMenu.vue";
+import MainFooter from "../components/MainFooter.vue";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     Header,
     MainBanner,
-    MainFooter
-  }
-
-}
+    MainFooter,
+    AskMenu,
+  },
+};
 </script>
 
 <style>
 #HomePage {
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
 }
 .navbar-area.is-sticky {
   top: 0;
@@ -43,15 +40,13 @@ export default {
   z-index: 999;
   position: fixed;
   -webkit-box-shadow: 0 2px 28px 0 rgba(0, 0, 0, 0.09);
-          box-shadow: 0 2px 28px 0 rgba(0, 0, 0, 0.09);
+  box-shadow: 0 2px 28px 0 rgba(0, 0, 0, 0.09);
   -webkit-animation: 500ms ease-in-out 0s normal none 1 running fadeInDown;
-          animation: 500ms ease-in-out 0s normal none 1 running fadeInDown;
+  animation: 500ms ease-in-out 0s normal none 1 running fadeInDown;
 }
 
 .edumim-nav .navbar {
   position: inherit;
   padding: 0;
 }
-  
 </style>
-  
