19c2913290b25dadedd9330930e07695e60630ef
opang <111403917+Leshangpang@users.noreply.github.com>
2024-09-09 18:27:17 +1000

    [Task-127] Banner for badge page <<added>>

---
+++ itp-front/src/components/ProfileBanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="profile-banner">
+      <img src="../assets/image/ProfileBanner.png" alt="ProfileBanner" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ProfileBanner",
+  };
+  </script>
+  
+  <style scoped>
+  /* Styling for Profile Banner */
+  .profile-banner {
+    text-align: center;
+    margin: 20px 0; /* Adjust margin to suit your layout */
+  }
+  
+  .profile-banner img {
+    max-width: 100%;
+    height: auto;
+    object-fit: contain; /* Ensures the image fits nicely */
+  }
+  </style>
--- itp-front/src/views/BadgesPage.vue
+++ itp-front/src/views/BadgesPage.vue
@@ -1,36 +1,48 @@
 <template>
-    <div id="BadgesPage">
-      <Header></Header>
-      <main class="content">
-        BadgePage
-        <router-view></router-view>
-      </main>
-      <Footer class="footer"></Footer>
-    </div>
-  </template>
+  <div id="BadgesPage">
+    <Header></Header>
+    
+    <main class="content">
+      <!-- add banner -->
+      <ProfileBanner></ProfileBanner>
+      <router-view></router-view>
+    </main>
+
+    <MainFooter></MainFooter>
+    <Footer class="footer"></Footer>
+    
+  </div>
+</template>
 
 <script>
 import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
+import MainFooter from '@/components/MainFooter.vue';
+import ProfileBanner from '@/components/ProfileBanner.vue';
 
 
 
 export default {
-  name: 'BadgePage',
-  components: {
-    Header,
-    Footer
-  }
+name: 'BadgePage',
+components: {
+  Header,
+  ProfileBanner,
+  MainFooter,
+}
 
 }
 </script>
 
 <style>
 #BadgePage {
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; 
 }
-  
+
+.content {
+  flex: 1; 
+  padding: 20px;
+}
+
+
 </style>
-  
