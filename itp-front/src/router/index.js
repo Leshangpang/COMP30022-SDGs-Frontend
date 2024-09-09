fcac355b8629ec976d888d0a99da4eca1c22f7f4
opang <111403917+Leshangpang@users.noreply.github.com>
2024-09-09 19:43:41 +1000

    [TASK-144] Implement Certificate Page <<added>> header and footer

--- itp-front/src/router/index.js
+++ itp-front/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import CoursePage from '../views/CoursePage.vue';
 import BadgesPage from '../views/BadgesPage.vue';
-import CertificationPage from '../views/CertificationPage.vue';
+import CertificatePage from '../views/CertificatePage.vue';
 import LearningPage from '../views/LearningPage.vue';
 import CommunityPage from '../views/CommunityPage.vue';
 
@@ -18,7 +18,7 @@ const routes = [
   { path: '/course', name: 'Course', component: CoursePage },
   { path: '/learning', name: 'learn', component: LearningPage },
   { path: '/badges', name: 'Badges', component: BadgesPage },
-  { path: '/certification', name: 'Certification', component: CertificationPage },
+  { path: '/certificate', name: 'Certificate', component: CertificatePage },
   { path: '/communitychallenge', name: 'CommunityChallenge', component: CommunityPage }
 
 
---
+++ itp-front/src/views/CertificatePage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="CertificatePage">
+    <Header></Header>
+    
+    <BadgeBanner></BadgeBanner>
+
+    <main class="certificate-content">
+      <section class="certificate">
+        
+      </section>
+    </main>
+
+    <MainFooter></MainFooter>
+    <Footer class="footer"></Footer>
+    
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import MainFooter from '@/components/MainFooter.vue';
+import CertificateBanner from '@/components/CertificateBanner.vue';
+
+
+
+export default {
+name: 'CertificatePage',
+components: {
+  Header,
+  CertificateBanner,
+  MainFooter,
+}
+
+}
+</script>
+
+<style>
+#CertificatePage {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; 
+}
+
+.content {
+  flex: 1; 
+  padding: 20px;
+}
+
+
+
+</style>
