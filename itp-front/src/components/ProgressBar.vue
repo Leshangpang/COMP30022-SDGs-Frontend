0bdd4bcc3b08bb7e35e1d45920ca28d2b553f177
ruipu cui <ruipucui@ruipus-MacBook-Air.local>
2024-09-15 18:32:15 +1000

    [TASK-219] <<added>> progress bar in the learning page

---
+++ itp-front/src/components/ProgressBar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="progress-bar">
+      <div class="progress-item" v-for="(item, index) in progressItems" :key="index">
+        <div class="progress-number">{{ item.number }}</div>
+        <div class="progress-label">{{ item.label }}</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  data() {
+    return {
+      progressItems: [
+        { number: 10, label: "topic" },
+        { number: 91, label: "flashcard" },
+        { number: 47, label: "quiz" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.progress-bar {
+  display: flex;
+  flex-direction: row; /* Ensure items are placed horizontally */
+  justify-content: space-around; /* Evenly space items */
+  align-items: center; /* Center items vertically */
+  padding: 10px 20px;
+  background-color: #fff;
+  border-radius: 15px;
+}
+
+.progress-item {
+  text-align: center;
+  flex: 1;
+}
+
+.progress-number {
+  font-size: 24px;
+  color: #6ec8c4;
+}
+
+.progress-label {
+  font-size: 14px;
+  color: #333;
+}
+
+.progress-item:not(:last-child) {
+  border-right: 1px solid #e5e5e5;
+}
+</style>
+
+
+  
+  
+  
+  
+  
--- itp-front/src/views/LearningContentPage.vue
+++ itp-front/src/views/LearningContentPage.vue
@@ -1,8 +1,14 @@
 <template>
-    <div id="TakeQuizPage">
+    <div id="CommunityChallenge">
       <Header></Header>
       <MainBanner></MainBanner>
-      <SideBar class="sidebar"></SideBar>
+      <div style="height: 20px;"></div>
+      <ProgressBar class="progress-bar"></ProgressBar>
+      <div class="sidebar-container">
+        <SideBar class="sidebar"></SideBar>
+        
+      </div>
+      
       <MainFooter></MainFooter>
  
       <main class="content">
@@ -13,38 +19,56 @@
   </template>
 
 <script>
-import Header from '../components/Header.vue';
 import MainBanner from '../components/MainBannerLearn.vue'
 import MainFooter from '../components/MainFooter.vue';
-import SideBar from '../components/SideBar.vue'
+import SideBar from '../components/SideBar.vue';
+import QuestionList from '../components/QuestionList.vue'
+import Header from '../components/Header.vue';
+import ProgressBar from '../components/ProgressBar.vue';
+
 
 
 
 
 export default {
-  name: 'TakeQuizPage',
+  name: 'CommunityChallenge',
   components: {
     Header,
     MainBanner,
     MainFooter,
     SideBar,
-
+    ProgressBar,
+    QuestionList,
   }
 
 }
 </script>
 
 <style scoped>
-#TakeQuizPage {
+#CommunityChallenge {
     display: grid;
     grid-template-rows: auto 1fr auto;
     min-height: 100vh;
 }
 
-.sidebar {
-  margin-left: 100px; /* 调整这个值来设置距离页面左边的边距 */
-  margin-top: 100px;
-  margin-bottom: 100px
+.progress-bar{
+  border-radius: 5px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+  width: 50%;
+  margin: 20px auto;
+  padding: 10px 0;
 }
-  
+
+.sidebar-container {
+  display: flex;
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+.sidebar {
+  margin:0;
+  margin-left: 100px;
+  flex: 1;
+}
+
 </style>
+  
