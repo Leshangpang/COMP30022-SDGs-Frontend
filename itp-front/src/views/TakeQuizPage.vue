7763f4e24d8ce463dd20edf9e8bfe7021a45018e
Wj12344 <jinwang8@student.unimelb.edu.au>
2024-09-17 00:57:01 +1000

    [TASK-224] <<update>>  Change the backgound picture and layout with the suggestion

--- itp-front/src/views/TakeQuizPage.vue
+++ itp-front/src/views/TakeQuizPage.vue
@@ -1,40 +1,42 @@
 <template>
-    <div id="TakeQuizPage">
-      <Header></Header>
-      <MainBanner></MainBanner>
-      <EachGoalBar></EachGoalBar>
-      <div class="main-content">
-        <SideBar class="sidebar"></SideBar>
-        <MultipleChoice></MultipleChoice>
-        <div class="catton"></div>
-      </div>
-      <MainFooter></MainFooter>
- 
-      <main class="content">
-        <router-view></router-view>
-      </main>
-
+  <div id="TakeQuizPage">
+    <Header></Header>
+    <MainBanner></MainBanner>
+    <EachGoalBar></EachGoalBar>
+    <div class="main-content">
+      <SideBar class="sidebar"></SideBar>
+      <MultipleChoice></MultipleChoice>
+      <div class="catton"></div>
     </div>
-  </template>
+    <MainFooter></MainFooter>
+
+    <main class="content">
+      <router-view></router-view>
+    </main>
+
+  </div>
+</template>
 
 <script>
+import EachGoalBar from '@/components/EachGoalBar.vue';
 import EachGoalBar from '@/components/EachGoalBar.vue';
 import Header from '../components/Header.vue';
 import MainBanner from '../components/MainBannerLearn.vue'
 import MainFooter from '../components/MainFooter.vue';
 import SideBar from '../components/SideBar.vue'
 import MultipleChoice from '@/components/MultipleChoice.vue';
+import MultipleChoice from '@/components/MultipleChoice.vue';
 
 
 export default {
-  name: 'TakeQuizPage',
-  components: {
-    Header,
-    MainBanner,
-    MainFooter,
-    SideBar,
-    EachGoalBar,
-    MultipleChoice
+name: 'TakeQuizPage',
+components: {
+  Header,
+  MainBanner,
+  MainFooter,
+  SideBar,
+  EachGoalBar,
+  MultipleChoice
 }
 
 }
@@ -42,29 +44,29 @@ export default {
 
 <style scoped>
 #TakeQuizPage {
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
 }
 
 .sidebar {
-  margin-left: 100px; /* 调整这个值来设置距离页面左边的边距 */
-  margin-top: 100px;
-  margin-bottom: 100px
+margin-left: 100px; /* 调整这个值来设置距离页面左边的边距 */
+margin-top: 100px;
+margin-bottom: 100px
 }
 
 .catton{
-  margin-right: 40px;
-  margin-left: 40px;
-  margin-top: 20px;
-  width: 180px ;
-  height: 480px;
-  background-color: hsl(0, 0%, 100%);
-  margin-bottom: 50px;
-  background-image: url('../assets/image/Goal5_Decor.jpg');
-  background-size: cover; 
-  background-position: center;
-  background-repeat: no-repeat;
-  transform: rotateY(180deg);
+margin-right: 40px;
+margin-left: 40px;
+margin-top: 20px;
+width: 180px ;
+height: 480px;
+background-color: hsl(0, 0%, 100%);
+margin-bottom: 50px;
+background-image: url('../assets/image/Goal5_Decor.jpg');
+background-size: cover; 
+background-position: center;
+background-repeat: no-repeat;
+transform: rotateY(180deg);
 }
 </style>
