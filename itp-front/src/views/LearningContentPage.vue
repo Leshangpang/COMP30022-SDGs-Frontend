187e9dca36188842c4be389b49b643e4e5b69b03
ruipu cui <ruipucui@ruipus-MacBook-Air.local>
2024-09-16 14:23:48 +1000

    [TASK-219]<<added>> the first and second topics

--- itp-front/src/views/LearningContentPage.vue
+++ itp-front/src/views/LearningContentPage.vue
@@ -15,8 +15,26 @@
       <ProgressBar class="progress-bar"></ProgressBar>
       <div class="sidebar-container">
         <SideBar class="sidebar"></SideBar>
-        <div class="content-container">
+
+        <div class="content-container" v-if="topicNum == 1">
+            <div class="text-container">
+                <h2>Overview</h2>
+                <p>Gender bias is undermining our social fabric and devalues all of us. It is not just a human rights issue;
+                     it is a tremendous waste of the world's human potential. By denying women equal rights, we deny half the 
+                     population a chance to live life at its fullest. Political, economic and social equality for women will 
+                     benefit all the world's citizens. Together we can eradicate prejudice and work for equal rights and 
+                     respect for all.
+                </p>
+            </div>
+            <div class="content-image-container">
+                <h2>Current Progress</h2>
+                <img src="../assets/image/SDG_report_2023_infographics_Goal 5.png" />
+            </div>
+            <div class="video-title">
+                <h2>Understand the SDG Goal</h2>
+            </div>
             <div class="video-container">
+                <h2>Understand the SDG Goal</h2>
                 <iframe
                 width="560"
                 height="315"
@@ -27,6 +45,81 @@
                 ></iframe>
             </div>
         </div>
+
+        <div class="content-container" v-if="topicNum == 2">
+            <div class="text-container">
+                <h2>The Targets</h2>
+                <p>Everyone can help to make sure that we meet the Global Goals. Use these nine targets 
+                    to create action for gender equality.
+                </p>
+            </div>
+
+            <div class="text-container">
+                <h4>5.1 END DISCRIMINATION AGAINST WOMEN AND GIRLS</h4>
+                <p>End allforms of discrimination against all women and girls everywhere.</p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.2 END ALL VIOLENCE AGAINST AND EXPLOITATION OF WOMEN AND GIRLS</h4>
+                <p>Eliminate all forms of violence against all women and girls in the public and private 
+                    spheres, including trafficking and sexual and other types of exploitation.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.3 ELIMINATE FORCED MARRIAGES AND GENITAL MUTILATION</h4>
+                <p>Eliminate all harmful practices, such as child, early and forced marriage and female genital mutilation.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.4 VALUE UNPAID CARE AND PROMOTE SHARED DOMESTIC RESPONSIBILITIES</h4>
+                <p>Recognize and value unpaid care and domestic work through the provision of public services, infrastructure 
+                    and social protection policies and the promotion of shared responsibility within the household and the family 
+                    as nationally appropriate.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.5 ENSURE FULL PARTICIPATION IN LEADERSHIP AND DECISION-MAKING</h4>
+                <p>Ensure women's full and effective participation and equal opportunities for leadership at all levels of 
+                    decision-making in political, economic and public life.
+                </p>
+                <div style="height: 15px;"></div>
+                
+                <h4>5.6 UNIVERSAL ACCESS TO REPRODUCTIVE HEALTH AND RIGHTS</h4>
+                <p>Ensure universal access to sexual and reproductive health and reproductive rights as agreed in accordance with 
+                    the Programme of Action of the International Conference on Population and Development and the Beijing Platform 
+                    for Action and the outcome documents of their review conferences.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.7 EQUAL RIGHTS TO ECONOMIC RESOURCES, PROPERTY OWNERSHIP AND FINANCIAL SERVICES</h4>
+                <p>Undertake reforms to give women equal rights to economic resources, as well as access to 
+                    ownership and control over land and other forms of property, financial services, inheritance 
+                    and natural resources, in accordance with national laws.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.8 PROMOTE EMPOWERMENT OF WOMEN THROUGH TECHNOLOGY</h4>
+                <p>Enhance the use of enabling technology, in particular information
+                     and communications technology, to promote the empowerment of women.
+                </p>
+                <div style="height: 15px;"></div>
+
+                <h4>5.9 ADOPT AND STRENGTHEN POLICIES AND ENFORCEABLE LEGISLATION FOR GENDER EQUALITY</h4>
+                <p>Adopt and strengthen sound policies and enforceable legislation for the promotion of gender equality
+                     and the empowerment of all women and girls at all levels
+                </p>
+                <div style="height: 15px;"></div>
+            </div>
+                
+        </div>
+
+        <div class="content-container" v-if="topicNum == 3">
+            <div class="text-container">
+                <h2>Things To Do</h2>
+            </div>
+
+
+        </div>
+
       </div>
       
       <MainFooter></MainFooter>
@@ -56,6 +149,7 @@ export default {
   },
   data() {
     return {
+        topicNum: 3,
         videoUrl: "https://www.youtube.com/embed/-hc0kZh6CnM",
     }
   }
@@ -108,7 +202,7 @@ export default {
 }
 
 .goal-text-container {
-  background-color: #E1503A;
+  background-color: #DC462E;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -134,10 +228,10 @@ export default {
 
 .content-container {
   display: flex;
-  flex-direction: column; /* 让问题在右侧垂直排列 */
+  flex-direction: column;
   margin-left: 70px;
   flex: 2; /* 控制右侧问题部分的宽度 */
-  max-height: 400px; /* 控制右侧问题列表的最大高度 */
+  max-height: 800px; /* 控制右侧问题列表的最大高度 */
   overflow-y: auto; /* 让问题列表可垂直滚动 */
   padding-right: 10px; /* 防止滚动条遮挡内容 */
 }
@@ -145,7 +239,7 @@ export default {
 .video-container {
   max-width: 100%;
   overflow: hidden;
-  padding-bottom: 56.25%; /* 16:9 aspect ratio */
+  padding-bottom: 56.25%;
   position: relative;
   height: 0;
 }
@@ -156,6 +250,22 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
+}
+
+.text-container{
+    padding-bottom: 40px;
+}
+
+.content-image-container{
+    padding-bottom: 40px;
+}
+
+.video-title{
+    padding-bottom: 20px;
+}
+
+h4{
+    font-size: 18px;
 }
 
 </style>
