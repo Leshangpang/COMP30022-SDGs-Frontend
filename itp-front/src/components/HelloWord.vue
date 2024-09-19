6a572c83c1042053e763ce9f2dc6256aea3751c3
YuetongLiiii <yuetong4@student.unimelb.edu.au>
2024-09-19 18:02:04 +1000

    [TASK-236] build test enviroment

---
+++ itp-front/src/components/HelloWord.vue
@@ -0,0 +1,27 @@
+<!-- HelloWorld.vue -->
+<template>
+    <div>
+      <p>{{ msg }}</p>
+      <button @click="increment">Click me</button>
+      <p>{{ count }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      increment() {
+        this.count++;
+      }
+    }
+  };
+  </script>
+  
