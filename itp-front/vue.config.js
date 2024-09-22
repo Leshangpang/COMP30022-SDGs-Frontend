f916473379d74061d609cd401869f8856d71b29f
opang <111403917+Leshangpang@users.noreply.github.com>
2024-09-22 16:49:03 +1000

    Save changes before pulling gh-pages

--- itp-front/vue.config.js
+++ itp-front/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/COMP30022-SDGs-Frontend/'  // Ensure this matches your GitHub repository name
+    : '/',
+  outputDir: 'itp-front'  // This specifies that the build output should be in the 'itp-front' folder
 })
