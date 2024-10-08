eef286281be9370657f126b6400b491d90eda054
opang <111403917+Leshangpang@users.noreply.github.com>
2024-10-08 22:41:14 +1100

    [TASK-275] <<removed>> global element ui imports in main.js

--- itp-front/src/main.js
+++ itp-front/src/main.js
@@ -14,10 +14,7 @@ import BootstrapVue from 'bootstrap-vue';
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 // use element ui
-Vue.use(ElementUI);
 Vue.use(BootstrapVue);
 
 Vue.use(VueAwesomeSwiper);
