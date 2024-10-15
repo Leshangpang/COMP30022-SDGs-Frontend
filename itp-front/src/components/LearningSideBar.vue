d79f3cad181d347d725c73549952c31f4d13326d
YuetongLiiii <yuetong4@student.unimelb.edu.au>
2024-10-16 02:11:41 +1100

    [TASK-293]adjust css 5

--- itp-front/src/components/LearningSideBar.vue
+++ itp-front/src/components/LearningSideBar.vue
@@ -1,54 +1,45 @@
 <template>
   <ul class="learning-modules">
-      <div class ="choose-section">
-        <el-dropdown @command="handleDropdownCommand">
-          <li 
-          @click="$emit('update', 1)"
-          :class="{ active: active === 1 }"
-          class="choose el-dropdown-link">
-          <router-link to="" data-text="Learning" >
-            Learning 
-            <i class="bx bx-chevron-down"></i> 
+    <div class="choose-section">
+      <el-dropdown @command="handleDropdownCommand">
+        <li @click="$emit('update', 1)" :class="{ active: active === 1 }" class="choose el-dropdown-link">
+          <router-link to="" data-text="Learning">
+            Learning
+            
           </router-link>
+          <i class="bx bx-chevron-down menu-arrow"></i>
           <!-- <span><i class="el-icon-arrow-down el-icon--right"></i></span> -->
           <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item command="1">Overview</el-dropdown-item>
-        <el-dropdown-item command="2">Targets</el-dropdown-item>
-        <el-dropdown-item command="3">Actions</el-dropdown-item>
-        <el-dropdown-item command="4">Events and News</el-dropdown-item>
-        <!-- <el-dropdown-item disabled>dd</el-dropdown-item> -->
-        <!-- <el-dropdown-item divided command="4">Divided Item</el-dropdown-item> -->
-      </el-dropdown-menu>
-          </li>
-        </el-dropdown>
-      </div>
+            <el-dropdown-item command="1">Overview</el-dropdown-item>
+            <el-dropdown-item command="2">Targets</el-dropdown-item>
+            <el-dropdown-item command="3">Actions</el-dropdown-item>
+            <el-dropdown-item command="4">Events and News</el-dropdown-item>
+            <!-- <el-dropdown-item disabled>dd</el-dropdown-item> -->
+            <!-- <el-dropdown-item divided command="4">Divided Item</el-dropdown-item> -->
+          </el-dropdown-menu>
+        </li>
+      </el-dropdown>
+    </div>
 
-      <div class ="choose-section">
-          <li 
-          @click="$emit('update', 2)"
-          :class="{ active: active === 2 }"
-          class="choose">
-          <router-link v-if= "isLoggedIn" to="flashcard" data-text="FlashCard">FlashCard</router-link>
-          <a v-else  @click="showLoginForm(false)">FlashCard</a>
-        </li>
-      </div>
-      <div class ="choose-section">
-          <li 
-          @click="$emit('update', 3)"
-          :class="{ active: active === 3 }"
-          class="choose">
-          <router-link v-if="isLoggedIn" to="quiz" data-text="Quiz">Quiz</router-link>
-          <a v-else  @click="showLoginForm(false)">Quiz</a></li>
-        </div>
-      <div class ="choose-section">
-          <li 
-          @click="$emit('update', 4)"
-          :class="{ active: active === 4 }"
-          class="choose">
-          <router-link v-if="isLoggedIn" to="/communitychallenge" data-text="Community Chanllenge">Community Challenge</router-link>
-          <a v-else  @click="showLoginForm(false)">Community Challenge</a>
-        </li>
-      </div>
+    <div class="choose-section">
+      <li @click="$emit('update', 2)" :class="{ active: active === 2 }" class="choose">
+        <router-link v-if="isLoggedIn" to="flashcard" data-text="FlashCard">FlashCard</router-link>
+        <a v-else @click="showLoginForm(false)">FlashCard</a>
+      </li>
+    </div>
+    <div class="choose-section">
+      <li @click="$emit('update', 3)" :class="{ active: active === 3 }" class="choose">
+        <router-link v-if="isLoggedIn" to="quiz" data-text="Quiz">Quiz</router-link>
+        <a v-else @click="showLoginForm(false)">Quiz</a>
+      </li>
+    </div>
+    <div class="choose-section">
+      <li @click="$emit('update', 4)" :class="{ active: active === 4 }" class="choose">
+        <router-link v-if="isLoggedIn" to="/communitychallenge" data-text="Community Chanllenge">Community
+          Challenge</router-link>
+        <a v-else @click="showLoginForm(false)">Community Challenge</a>
+      </li>
+    </div>
   </ul>
 </template>
 
@@ -56,64 +47,64 @@
 import { EventBus } from '@/eventBus';
 
 export default {
-name: "SideBar",
-props: {
-  active: {
-    type: Number,
-    default: 1,
+  name: "SideBar",
+  props: {
+    active: {
+      type: Number,
+      default: 1,
+    },
   },
-},
-emits: ["update"],
-components: {},
-data() {
-  return {
-    isLoggedIn: true, // initialise to not logged
-  };
-},
-created() {
+  emits: ["update"],
+  components: {},
+  data() {
+    return {
+      isLoggedIn: true, // initialise to not logged
+    };
+  },
+  created() {
     // this.checkLoginStatus();
     window.addEventListener('scroll', this.handleScroll);
   },
   destroyed() {
     window.removeEventListener('scroll', this.handleScroll);
   },
-methods: {
-  navigateToLearning() {
-    this.$router.push({ name: "/learning" });
-  },
-  handleDropdownCommand(command) {
-    const routes = {
-    "1": { path: "/learning", query: { topic: "overview" } },
-    "2": { path: "/learning", query: { topic: "targets" } },
-    "3": { path: "/learning", query: { topic: "actions" } },
-    "4": { path: "/learning", query: { topic: "events and news" } },
-    };
-    const targetRoute = routes[command];
+  methods: {
+    navigateToLearning() {
+      this.$router.push({ name: "/learning" });
+    },
+    handleDropdownCommand(command) {
+      const routes = {
+        "1": { path: "/learning", query: { topic: "overview" } },
+        "2": { path: "/learning", query: { topic: "targets" } },
+        "3": { path: "/learning", query: { topic: "actions" } },
+        "4": { path: "/learning", query: { topic: "events and news" } },
+      };
+      const targetRoute = routes[command];
 
-    if (this.$route.path !== targetRoute.path || this.$route.query.topic !== targetRoute.query.topic) {
-      this.$router.push(targetRoute);
-    }
-  },
-  handleScroll() {
+      if (this.$route.path !== targetRoute.path || this.$route.query.topic !== targetRoute.query.topic) {
+        this.$router.push(targetRoute);
+      }
+    },
+    handleScroll() {
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
       this.isSticky = scrollTop > 50; // Adjust as needed
-  },
-  showLoginForm(isSignUp) {
-    this.isSignUp = isSignUp;
-    EventBus.$emit('toggle-login-form', this.isSignUp);
-  },
-  checkLoginStatus() {
+    },
+    showLoginForm(isSignUp) {
+      this.isSignUp = isSignUp;
+      EventBus.$emit('toggle-login-form', this.isSignUp);
+    },
+    checkLoginStatus() {
       // 假设已经登录，用户名为 "Alice"
       this.isLoggedIn = true; // 设置为已登录
       this.username = 'Alice'; // 从后端获取的用户名
-  },
+    },
   },
 };
 </script>
 
 <style scoped>
 .learning-modules {
-height: 400px;
+  height: 400px;
   border-radius: 15px;
   background-color: rgba(197, 246, 250, 0.26);
   display: flex;
@@ -126,36 +117,50 @@ height: 400px;
   padding: 69px 24px 69px 30px;
   font: 400 36px Inter, sans-serif;
   border: 2px solid #c5f6fa;
-  
- 
-}
-.choose-section{
-display: flex;
-width: 100%;
-max-width: 207px;
-gap: 22px;
-margin-bottom: 20px;
+
 
 }
+
+.choose-section {
+  display: flex;
+  width: 100%;
+  max-width: 207px;
+  gap: 22px;
+  margin-bottom: 20px;
+
+}
+
 .choose {
-color: #8a8686;
-transition: background-color 0.3s ease, color 0.3s ease;
+ 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  color: #8a8686;
+  transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+.menu-arrow {
+  font-size: 24px;   /* 增大箭头的字体大小，使其变大 */
 }
 
 
 
 .choose a {
-  color: inherit; /* Inherit the color from the parent element */
-  text-decoration: none; /* Remove the underline */
-  display: block; /* Make the link fill the entire li area */
-  padding: 10px; /* Add padding to increase the clickable area */
+  color: inherit;
+  /* Inherit the color from the parent element */
+  text-decoration: none;
+  /* Remove the underline */
+  display: block;
+  /* Make the link fill the entire li area */
+  padding: 10px;
+  /* Add padding to increase the clickable area */
   transition: color 0.3s ease;
   font-size: 23px;
 }
 
 .choose:hover a {
-  color: black; /* Change the link text color to black when hovered */
+  color: black;
+  /* Change the link text color to black when hovered */
 }
-
-
 </style>
