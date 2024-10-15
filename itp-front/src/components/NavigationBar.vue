8595adfd8e31abdcd8c49b1106fcf8266d680e72
ruipu cui <ruipucui@ruipus-MacBook-Air.local>
2024-10-16 10:50:33 +1100

    [TASK-292] <<fixed>> bug for the progress bar

--- itp-front/src/components/NavigationBar.vue
+++ itp-front/src/components/NavigationBar.vue
@@ -233,6 +233,10 @@ export default {
   },
 
   mounted() {
+    const storedProgress = localStorage.getItem('totalProgress');
+    this.percentage = storedProgress !== null ? storedProgress : 0;
+
+
     EventBus.$on('loginStatusChanged', (status) => {
       this.isLoggedIn = status;
     });
